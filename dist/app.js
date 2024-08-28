258f8561c78e7ae80823e3c866b9d7b3de8cbedd
abdalla-ayman <abdallaaymanshandi3@gmail.com>
2024-08-28 18:22:42 +0200

    creating basic project structure

---
+++ dist/app.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const winston_1 = __importDefault(require("winston"));
+const express_winston_1 = __importDefault(require("express-winston"));
+const debug_1 = __importDefault(require("debug"));
+const helmet_1 = __importDefault(require("helmet"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const dotenvResult = dotenv_1.default.config();
+if (dotenvResult.error) {
+    throw dotenvResult.error;
+}
+const app = (0, express_1.default)();
+const port = 3000 || process.env.PORT;
+const debugLog = (0, debug_1.default)('app');
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// here we are preparing the expressWinston logging middleware configuration,
+// which will automatically log all HTTP requests handled by Express.js
+const loggerOptions = {
+    transports: [new winston_1.default.transports.Console()],
+    format: winston_1.default.format.combine(winston_1.default.format.json(), winston_1.default.format.prettyPrint(), winston_1.default.format.colorize({ all: true })),
+};
+if (!process.env.DEBUG) {
+    loggerOptions.meta = false; // when not debugging, log requests as one-liners
+}
+// initialize the logger with the above configuration
+app.use(express_winston_1.default.logger(loggerOptions));
+const runningMessage = `Server running at http://localhost:${port}`;
+app.listen(port, () => {
+    debugLog("server is running");
+    console.log(runningMessage);
+});
---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+import cors from 'cors'
+import winston from 'winston';
+import expressWinston from 'express-winston';
+import debug from 'debug'
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+    throw dotenvResult.error;
+}
+const app: express.Application = express()
+const port = 3000 || process.env.PORT
+const debugLog: debug.IDebugger = debug('app')
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+
+// here we are preparing the expressWinston logging middleware configuration,
+// which will automatically log all HTTP requests handled by Express.js
+const loggerOptions: expressWinston.LoggerOptions = {
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({ all: true })
+    ),
+};
+
+if (!process.env.DEBUG) {
+    loggerOptions.meta = false; // when not debugging, log requests as one-liners
+}
+
+// initialize the logger with the above configuration
+app.use(expressWinston.logger(loggerOptions));
+
+const runningMessage = `Server running at http://localhost:${port}`;
+app.listen(port, () => {
+    debugLog("server is running")
+    console.log(runningMessage)
+
+})
+
+
